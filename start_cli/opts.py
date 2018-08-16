0af6d62f4c90a382ab13858f2f2c13ca63448c04
ChrisTimperley <christimperley@googlemail.com>
2018-08-15 21:20:31 -0400

    renamed options

--- start_cli/opts.py
+++ start_cli/opts.py
@@ -1,6 +1,6 @@
 OPT_FILE = (['file'], {'help': "path to the scenario config file"})
 OPT_TIMEOUT = \
-    (['--timeout'],
+    (['--time-limit'],
      {'help': 'the number of seconds that may pass without success until a mission is considered a failure.',
      'type': int,
      'default': 300})
@@ -13,13 +13,14 @@ OPT_CHECK_WAYPOINTS = \
     (['--check-wps'],
      {'help': 'enable checking of number of visited waypoints.',
       'action': 'store_true'})
+OPT_SITL_PREFIX = None
 OPT_SPEEDUP = \
     (['--speedup'],
      {'help': 'the speed-up factor that should be applied to the simulation clock.',
       'type': int,
-      'default': 30})
+      'default': 10})
 OPT_LIVENESS = \
-    (['--timeout-liveness'],
+    (['--liveness-timeout'],
      {'help': 'the number of seconds that may pass without communication with the rover until the mission is aborted.',
       'type': int,
-      'default': 30})
+      'default': 1})
