7eea36c31753fec6adfc9018cf2eaefa06dc6683
ChrisTimperley <christimperley@googlemail.com>
2018-09-04 21:35:30 -0400

    added --docker-client option to image commands

--- start_cli/image.py
+++ start_cli/image.py
@@ -8,6 +8,7 @@ from start_image.build import build_scenario_image
 from start_image import install_from_archive, save_to_archive
 from start_core.scenario import Scenario
 from cement.ext.ext_argparse import ArgparseController, expose
+from docker import DockerClient
 
 from .opts import *
 
@@ -22,12 +23,17 @@ class ImageController(ArgparseController):
         stacked_on = 'base'
         stacked_type = 'embedded'
 
+    def obtain_docker(self):
+        # type: () -> DockerClient
+        return DockerClient(version=self.app.pargs.docker_client)
+
     @expose(
         help='builds the Docker image for a given scenario',
-        arguments=[OPT_FILE])
+        arguments=[OPT_FILE, OPT_DOCKER_CLIENT])
     def build(self):
         # type: () -> None
         fn_scenario = self.app.pargs.file
+        dkr = self.obtain_docker()
 
         logger.info("loading scenario from file [%s]", fn_scenario)
         scenario = Scenario.from_file(fn_scenario)
@@ -35,18 +41,20 @@ class ImageController(ArgparseController):
         name_image = image_name(scenario)
         logger.info("building image [%s] for scenario [%s]",
                     name_image, scenario.name)
-        build_scenario_image(scenario)
+        build_scenario_image(dkr, scenario)
         logger.info("built image [%s] for scenario [%s]",
                     name_image, scenario.name)
 
     @expose(
         help='installs the Docker image for a scenario from an archive',
         arguments=[OPT_FILE,
-                   OPT_ARCHIVE])
+                   OPT_ARCHIVE,
+                   OPT_DOCKER_CLIENT])
     def install(self):
         # type: () -> None
         fn_scenario = self.app.pargs.file
         fn_archive = self.app.pargs.fn_archive
+        dkr = self.obtain_docker()
 
         logger.info("loading scenario from file [%s]", fn_scenario)
         scenario = Scenario.from_file(fn_scenario)
@@ -56,7 +64,7 @@ class ImageController(ArgparseController):
         logger.info("installing Docker image [%s] for scenario [%s] from archive: %s",
                     image, scenario.name, fn_archive)
         try:
-            install_from_archive(scenario, fn_archive)
+            install_from_archive(dkr, scenario, fn_archive)
         except Exception:
             logger.exception("failed to install image from archive")
             raise
